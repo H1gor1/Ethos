8d780dc0cb0fee9ba455127b29292236d6ea873c
Higor <higorgabrieldev@gmail.com>
2026-05-13 22:12:58 -0300

    fix: all categories request

--- api/src/modules/category/dto/categoriesPaginatedRequest.ts
+++ api/src/modules/category/dto/categoriesPaginatedRequest.ts
@@ -2,8 +2,8 @@ import * as z from "zod";
 
 export const categoriesPaginatedSchema = z.object({
   search: z.string().optional().default(""),
-  skip: z.coerce.number().positive(),
-  take: z.coerce.number().positive(),
+  skip: z.coerce.number().positive().optional(),
+  take: z.coerce.number().positive().optional(),
 });
 
 export type categoriesPaginatedRequest = z.infer<typeof categoriesPaginatedSchema>;
